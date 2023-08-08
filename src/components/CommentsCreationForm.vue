e677bd95d2619401ad463bc8c04b755f85640deb
Vitor Rafael <rqfvitor@gmail.com>
2023-08-08 15:18:46 -0300

    lint: run lint to format code

--- src/components/CommentsCreationForm.vue
+++ src/components/CommentsCreationForm.vue
@@ -2,7 +2,10 @@
   export default {
     name: 'CommentsCreationForm',
     props: {
-      comments: Array
+      comments: {
+        type: Array,
+        default: () => []
+      }
     },
     data() {
       return {
@@ -33,25 +36,56 @@
 </script>
 
 <template>
-  <!-- Header Section -->
-  <div class="flex flex-col gap-1">
-        <h1 class="text-2xl font-semibold">Create a new comment</h1>
-        <p class="text-zinc-400">Insert your thoughts below to share them with anothers.</p>
-      </div>
+  <div class="flex flex-col gap-4">
+    <!-- Header Section -->
+    <div class="flex flex-col gap-1">
+      <h1 class="text-2xl font-semibold">
+        Create a new comment
+      </h1>
+      <p class="text-zinc-400">
+        Insert your thoughts below to share them with anothers.
+      </p>
+    </div>
       
-      <!-- Field Section -->
-      <div class="flex flex-col gap-2">
-        <label for="name" class="tracking-wide">Name</label>
-        <input type="text" id="name" name="name" class="px-4 py-3 rounded bg-zinc-700 outline-none focus:ring-2 focus:ring-blue-500" placeholder="Your name" v-model="name"/>
-        <small class="text-zinc-400">Empty will be posted as Anonymous</small>
-      </div>
+    <!-- Field Section -->
+    <div class="flex flex-col gap-2">
+      <label
+        for="name"
+        class="tracking-wide"
+      >Name</label>
+      <input
+        id="name"
+        v-model="name"
+        type="text"
+        name="name"
+        class="px-4 py-3 rounded bg-zinc-700 outline-none focus:ring-2 focus:ring-blue-500"
+        placeholder="Your name"
+      >
+      <small class="text-zinc-400">Empty will be posted as Anonymous</small>
+    </div>
 
-      <!-- Field Section -->
-      <div class="flex flex-col gap-2">
-        <label for="message" class="tracking-wide">Message<span class="text-red-500">*</span></label>
-        <textarea type="text" id="message" name="message" class="px-4 py-2 rounded bg-zinc-700 outline-none focus:ring-2 focus:ring-blue-500 resize-none h-[100px]" placeholder="Your message" v-model="message"></textarea>
-      </div>
+    <!-- Field Section -->
+    <div class="flex flex-col gap-2">
+      <label
+        for="message"
+        class="tracking-wide"
+      >Message<span class="text-red-500">*</span></label>
+      <textarea
+        id="message"
+        v-model="message"
+        type="text"
+        name="message"
+        class="px-4 py-2 rounded bg-zinc-700 outline-none focus:ring-2 focus:ring-blue-500 resize-none h-[100px]"
+        placeholder="Your message"
+      />
+    </div>
 
-      <!-- Submit Button -->
-      <button @click="createNewComment" class="tracking-wider uppercase py-2 px-4 rounded bg-blue-500">Submit</button>
+    <!-- Submit Button -->
+    <button
+      class="tracking-wider uppercase py-2 px-4 rounded bg-blue-500"
+      @click="createNewComment"
+    >
+      Submit
+    </button>
+  </div>
 </template>
--- src/components/CommentsForm.vue
+++ src/components/CommentsForm.vue
@@ -3,18 +3,11 @@
 
   export default {
     name: 'CommentsForm',
+    components: { CommentsCreationForm },
     data() {
         return {
             comments: [],
         };
-    },
-    methods: {
-      addComment(comment) {
-        this.comments.push(comment);
-      },
-      removeComment(id) {
-        this.comments = this.comments.filter(comment => comment.id !== id);
-      }
     },
     computed: {
         commentsCount() {
@@ -29,42 +22,76 @@
             });
         }
     },
-    components: { CommentsCreationForm }
+    methods: {
+      addComment(comment) {
+        this.comments.push(comment);
+      },
+      removeComment(id) {
+        this.comments = this.comments.filter(comment => comment.id !== id);
+      }
+    }
 }
 </script>
 
 <template>
   <!-- Wrapper -->
   <div class="flex w-[500px]">
-
     <!-- Container -->
     <div class="bg-zinc-800 border-2 border-zinc-700 rounded-md p-8 flex flex-col gap-4">
-      <CommentsCreationForm @new-comment="addComment" :comments="comments"/>
+      <CommentsCreationForm
+        :comments="comments"
+        @new-comment="addComment"
+      />
 
       <!-- Comments Section -->
       <div>
         <!-- Header Section -->
         <div class="flex flex-col gap-1 mb-2">
-          <h2 class="text-2xl font-semibold">Comments</h2>
-          <p v-if="comments.length == 0" class="text-zinc-400 text-sm">Nenhum comentário.</p>
-          <p v-else class="text-zinc-400 text-sm">Total: {{ commentsCount }}</p>
+          <h2 class="text-2xl font-semibold">
+            Comments
+          </h2>
+          <p
+            v-if="comments.length == 0"
+            class="text-zinc-400 text-sm"
+          >
+            Nenhum comentário.
+          </p>
+          <p
+            v-else
+            class="text-zinc-400 text-sm"
+          >
+            Total: {{ commentsCount }}
+          </p>
         </div>
 
         <!-- Comments -->
         <div class="flex flex-col gap-4 max-h-[200px] overflow-y-auto">
-
           <!-- Comment -->
-          <div class="flex flex-col gap-2 bg-zinc-700 px-4 py-2 rounded-md" v-for="comment in commentsWithNoName" v-bind:key="comment.id">
-
+          <div
+            v-for="comment in commentsWithNoName"
+            :key="comment.id"
+            class="flex flex-col gap-2 bg-zinc-700 px-4 py-2 rounded-md"
+          >
             <!-- User Info + Date -->
             <div class="flex justify-between items-center">
-              <h3 class="text-lg font-semibold">{{ comment.name }}</h3>
-              <p class="text-zinc-400 text-sm">{{ comment.createdAt }}</p>
+              <h3 class="text-lg font-semibold">
+                {{ comment.name }}
+              </h3>
+              <p class="text-zinc-400 text-sm">
+                {{ comment.createdAt }}
+              </p>
             </div>
 
             <!-- Message -->
-            <p class="text-zinc-400 max-w-[426px] break-words">{{ comment.message }}</p>
-            <button @click="removeComment(comment.id)" class="text-sm text-red-500 self-start">Remove</button>
+            <p class="text-zinc-400 max-w-[426px] break-words">
+              {{ comment.message }}
+            </p>
+            <button
+              class="text-sm text-red-500 self-start"
+              @click="removeComment(comment.id)"
+            >
+              Remove
+            </button>
           </div>
         </div>
       </div>

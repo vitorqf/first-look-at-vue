5ebd6e260ca7f0fcc71bc4754c7a134ae62148a2
Vitor Rafael <rqfvitor@gmail.com>
2023-08-08 14:01:10 -0300

    feat: create first comments section design

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CommentsSection from './components/CommentsSection';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CommentsSection
   }
 }
 </script>
 
+<template>
+  <div class="flex justify-center items-center bg-zinc-900 text-zinc-100 min-h-screen">
+    <CommentsSection />
+  </div>
+</template>
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CommentsSection.vue
@@ -0,0 +1,31 @@
+<script>
+  export default {
+    name: 'CommentsSection',
+  }
+</script>
+
+<template>
+  <div class="flex w-[500px]">
+    <form class="bg-zinc-800 border-2 border-zinc-700 rounded-md p-8 flex flex-col gap-4">
+      <div class="flex flex-col gap-1">
+        <h1 class="text-3xl font-semibold self-start">Comments</h1>
+        <p class="text-zinc-400">Insert your thoughts below to share them with anothers.</p>
+      </div>
+      
+      <div class="flex flex-col gap-2">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" class="px-4 py-3 rounded bg-zinc-700 outline-none focus:ring-2 focus:ring-blue-500" placeholder="Your name"/>
+      </div>
+
+      <div class="flex flex-col gap-2">
+        <label for="message">Message</label>
+        <textarea type="text" id="message" name="message" class="px-4 py-2 rounded bg-zinc-700 outline-none focus:ring-2 focus:ring-blue-500 resize-none h-[100px]" placeholder="Your message"></textarea>
+      </div>
+      <button type="submit" class="tracking-wider uppercase py-2 px-4 rounded bg-blue-500">Submit</button>
+    </form>
+  </div>
+</template>
+
+
+
+
